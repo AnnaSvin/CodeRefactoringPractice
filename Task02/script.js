03f86dc8719f7ee781d37c78174a3f9f4610e5b1
Lordenko <gameryt527@gmail.com>
2025-02-02 10:38:16 +0200

    add DRY Principle

--- Task02/script.js
+++ Task02/script.js
@@ -1,46 +1,45 @@
 class Slider {
+    createElement(tag, options = {}) {
+        const el = document.createElement(tag);
+        if (options.classes) {
+          options.classes.forEach(cls => el.classList.add(cls));
+        }
+        if (options.styles) {
+          Object.assign(el.style, options.styles);
+        }
+        if (options.id) {
+          el.id = options.id;
+        }
+        return el;
+    }
+
     constructor(direction = 'horizontal') {
         this.direction = direction;
         this.slides = [];
         this.currentIndex = 0;
 
-        this.block = document.createElement("div");
-        this.block.classList.add("block");
+        this.block = this.createElement("div", { classes: ["block"] });
+        
+        this.slider = this.createElement("div", { classes: ["slider", this.direction === 'vertical' ? "vertical" : "horizontal"] });
+        
+        this.container = this.createElement("div", { classes: ["container"] });
+        
+        this.sliderWrapper = this.createElement("div", { classes: ["wrapper", this.direction === 'vertical' ? "vertical" : "horizontal"] });
+        
+        this.panel = this.createElement("div", { classes: [this.direction === 'horizontal' ? 'horizontalPanel' : 'verticalPanel'] });
 
-        this.slider = document.createElement("div");
-        this.slider.classList.add("slider");
-        if (this.direction === 'vertical') {
-            this.slider.style.flexDirection = 'row';
-        }
-        this.container = document.createElement("div");
-        this.container.classList.add("container");
-        this.sliderWrapper = document.createElement("div");
-        this.sliderWrapper.classList.add("wrapper");
-        if (this.direction === 'vertical') {
-            this.sliderWrapper.style.flexDirection = 'column';
-        }
-
-        this.panel = document.createElement("div");
-        if (this.direction === 'horizontal') {
-            this.panel.classList.add('horizontalPanel');
-        }
-        else {
-            this.panel.classList.add('verticalPanel');
-        }
-
-        this.prevButton = document.createElement("button");
-        this.prevButton.classList.add("prevBtn");
+        this.prevButton = this.createElement("button", { classes: ["prevBtn"] });
         this.prevButton.textContent = "Prev";
 
-        this.fileInput = document.createElement('input');
+        const uniqueId = 'fileInput_' + Math.random().toString(36).substr(2, 9);
+        this.fileInput = this.createElement("input", { id: uniqueId });
         this.fileInput.type = 'file';
         this.fileInput.id = 'fileInput';
         this.fileInput.accept = 'image/*';
         this.fileInput.multiple = true;
         this.fileInput.addEventListener('change', (event) => this.uploadImages(event.target.files));
 
-        this.nextButton = document.createElement("button");
-        this.nextButton.classList.add("nextBtn");
+        this.nextButton = this.createElement("button", { classes: ["nextBtn"] });
         this.nextButton.textContent = "Next";
 
         this.prevButton.addEventListener("click", (event) => { this.prevSlide() });
